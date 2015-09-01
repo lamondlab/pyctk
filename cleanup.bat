8e1ffb72cf9ef467254f4e6a6be76eb4a78319e8
Rob Kent <jazzycamel@users.noreply.github.com>
2015-09-01 17:42:22 +0100

    Updates to build on win32

--- cleanup.bat
+++ cleanup.bat
@@ -1,8 +1,15 @@
 @echo off
 nmake clean
 rd modules /S /Q
-rd src\debug /S /Q
-rd src\release /S /Q
+
+rd src\Core\debug /S /Q
+rd src\Core\release /S /Q
+del src\Core\Makefile* /Q
+
+rd src\Widgets\debug /S /Q
+rd src\Widgets\release /S /Q
+del src\Widgets\Makefile* /Q
+
 del src\Makefile* /Q
 del Makefile /Q
 @echo on
