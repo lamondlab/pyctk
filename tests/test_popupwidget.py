aec8f95235a8957132b8790716b0cecb5993a901
Rob Kent <rob@gulon.co.uk>
2015-09-03 17:09:06 +0100

    Added resources and UI files. Added ctkAddRemoveComboBox and associated test.

--- tests/test_popupwidget.py
+++ tests/test_popupwidget.py
@@ -2,7 +2,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
-from PyCTK import ctkPopupWidget as PopupWidget
+from PyCTK.Widgets import ctkPopupWidget as PopupWidget
 
 class Widget(QWidget):
     def __init__(self, parent=None, **kwargs):
