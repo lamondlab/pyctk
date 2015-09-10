407a4692b53b1fec3cd1b51d65b098c749e80c44
Rob Kent <rob@gulon.co.uk>
2015-09-10 14:28:05 +0100

    Added ctkWidgets.h. Also added initCtkWidgetsResources() call in Widgets.sip such that Q_INIT_RESOURCES gets called on import of PyCTK.Widgets.

---
+++ src/Widgets/ctkWidgets.h
@@ -0,0 +1,11 @@
+#ifndef CTKWIDGETS_H
+#define CTKWIDGETS_H
+
+#include "../ctkExport.h"
+
+inline void CTK_EXPORT initCtkWidgetsResources(){
+    Q_INIT_RESOURCE(ctkWidgets);
+}
+
+#endif // CTKWIDGETS_H
+
